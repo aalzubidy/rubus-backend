0bacd5b41dfb883c03de0dc20020580cd30e3753
Ahmed Al-Zubidy <aalzubidy90@gmail.com>
2020-11-15 23:24:58 -0800

    Added login and logout

--- routes/authorization.js
+++ routes/authorization.js
@@ -2,6 +2,7 @@ const express = require("express");
 const jwt = require('jsonwebtoken');
 const db = require('../db/db');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
@@ -11,30 +12,46 @@ const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
  */
 router.post('/login', async (req, res) => {
   try {
-    // Login process to check username/password and update username below
+    // Check if there is no email or password
+    if (!req.body.email || !req.body.password) {
+      throw 'Please provide email and password';
+    }
 
-    const id = '1';
-    const name = 'Test User';
-    const email = 'a@a.com';
+    // Extract email and password
+    const { email } = req.body;
+    let { password } = req.body;
 
-    const user = { id, name, email };
+    // Query the database to get user information using the email
+    let queryResults = await db.query('select id, name, email, password from users where email=$1', [email]);
+
+    // If there is such user, extract the information
+    if (queryResults && queryResults.rows[0]) {
+      queryResults = queryResults.rows[0];
+    } else {
+      throw 'Please check email and password';
+    }
+
+    // Check the password against the hash
+    const passwordCheck = await bcrypt.compare(password, queryResults.password);
+
+    if (!passwordCheck) {
+      throw 'Please check email and password';
+    }
+
+    // Generate access token and refresh token
+    const user = { id: queryResults.id, name: queryResults.name, email };
 
     const accessToken = await jwt.sign(user, accessTokenSecret, { expiresIn: '30m' });
     const refreshToken = await jwt.sign(user, refreshTokenSecret);
 
-    const results = await db.query('update users set refresh_token=$1 where id=$2', [refreshToken, id]);
-    if (results) {
-      res.json({ accessToken, refreshToken });
-    } else {
-      const dbMsg = 'Could not update users';
-      console.log(dbMsg);
-      console.log(results);
-      throw dbMsg;
-    }
+    // Update the database with the new refresh token
+    await db.query('update users set refresh_token=$1 where id=$2', [refreshToken, queryResults.id]);
+
+    // Return the access token and the refresh token
+    res.json({ accessToken, refreshToken });
   } catch (error) {
-    const errorMsg = 'Could not login';
-    console.log(errorMsg);
-    console.log(error);
+    const errorMsg = 'Could not login :(';
+    console.log(errorMsg, error);
     res.send(500, errorMsg);
   }
 });
@@ -44,28 +61,33 @@ router.post('/login', async (req, res) => {
  */
 router.delete('/logout', async (req, res) => {
   try {
-    const accessResults = await jwt.verify(req.body.accessToken, accessTokenSecret);
-    const refreshResults = await jwt.verify(req.body.refreshToken, refreshTokenSecret);
-    if (accessResults.user.email === refreshResults.user.email) {
-      const dbResults = await db.query('delete from users where refresh_token=$1', [req.body.refreshToken]);
-      if (dbResults) {
-        res.json({ 'results': 'success' });
-      } else {
-        const dbMsg = 'Could not update users for logout';
-        console.log(dbMsg);
-        console.log(dbResults);
-        throw dbMsg;
-      }
+    // Extract token and refresh token
+    const { token } = req.headers;
+    const { refreshToken } = req.body;
+
+    if (!token || !refreshToken) {
+      throw 'Please provide token and refresh token'
+    }
+
+    // Verify both tokens
+    const tokenVerify = await jwt.verify(token, accessTokenSecret);
+    const refreshTokenVerify = await jwt.verify(refreshToken, refreshTokenSecret);
+
+    if(tokenVerify.id != refreshTokenVerify.id){
+      throw 'Please provide valid token and refresh token'
+    }
+
+    // Delete refresj token from database
+    const dbResults = await db.query('update users set refresh_token=$1 where refresh_token=$2', [null, refreshToken]);
+
+    if (dbResults) {
+      res.json({ 'results': 'Logged out successful' });
     } else {
-      const dbMsg = 'Could not verify tokens';
-      console.log(dbMsg);
-      console.log(dbResults);
-      throw dbMsg;
+      throw 'Could not delete token';
     }
   } catch (error) {
-    const errorMsg = 'Could not logout';
+    const errorMsg = `Could not logout ${error}`;
     console.log(errorMsg);
-    console.log(error);
     res.send(500, errorMsg);
   }
 })
