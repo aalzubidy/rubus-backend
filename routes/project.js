bc8cb6d37944d0f134ac574fb6d3e5efacceae6e
Ahmed Al-Zubidy <aalzubidy90@gmail.com>
2020-11-18 22:40:05 -0800

    Added get projects and project

--- routes/project.js
+++ routes/project.js
@@ -20,4 +20,38 @@ router.post('/newProject', async (req, res) => {
   }
 });
 
+/**
+ * @summary Get user's projects
+ */
+router.get('/projects', async (req, res) => {
+  try {
+    const user = await authorizationSrc.verifyToken(req);
+    const data = await projectSrc.getProjects(user);
+    res.status(200).json({
+      data
+    });
+  } catch (error) {
+    res.status(error.code).json({
+      error
+    });
+  }
+});
+
+/**
+ * @summary Get a single project
+ */
+router.get('/projects/:projectId', async (req, res) => {
+  try {
+    const user = await authorizationSrc.verifyToken(req);
+    const data = await projectSrc.getProject(req, user);
+    res.status(200).json({
+      data
+    });
+  } catch (error) {
+    res.status(error.code).json({
+      error
+    });
+  }
+});
+
 module.exports = router;
--- src/projectSrc.js
+++ src/projectSrc.js
@@ -4,6 +4,7 @@ const db = require('../db/db');
 /**
  * Create new project
  * @param {*} req http request contains project tile and description
+ * @param {object} user User information
  * @returns {object} newProjectResults
  * @throws {object} errorCodeAndMsg
  */
@@ -36,6 +37,101 @@ const newProject = async function newProject(req, user) {
   }
 };
 
+/**
+ * Get user projects
+ * @param {object} user User information
+ * @returns {array} projects list of projects and access level
+ * @throws {object} errorCodeAndMsg
+ */
+const getProjects = async function getProjects(user) {
+  try {
+    const {
+      id
+    } = user;
+
+    // Get projects from the database
+    let projectsQuery = await db.query('select project_id, added_by, projects_users.create_date, title, description from projects_users, projects where projects_users.user_id=$1 AND projects_users.project_id=projects.id', [id]);
+    if (!projectsQuery || !projectsQuery.rows || projectsQuery.rows.length <= 0) {
+      return [];
+    }
+
+    projectsQuery = projectsQuery.rows;
+
+    const allProjects = projectsQuery.map((item) => {
+      return {
+        projectId: item.project_id,
+        projectTitle: item.title,
+        projectDescription: item.description,
+        joinedDate: item.create_date,
+        admin: id === item.added_by ? true : false
+      };
+    });
+    return allProjects;
+  } catch (error) {
+    if (error.code) {
+      throw error;
+    }
+    const userMsg = 'Could not create project';
+    console.log(userMsg, error);
+    throw { code: 500, message: userMsg };
+  }
+};
+
+/**
+ * Get user projects
+ * @param {*} req Http request contains project id
+ * @param {object} user User information
+ * @returns {object} project
+ * @throws {object} errorCodeAndMsg
+ */
+const getProject = async function getProject(req, user) {
+  try {
+    const {
+      id
+    } = user;
+
+    const { projectId } = req.params;
+
+    if (!projectId) {
+      throw { code: 400, message: 'Please provide project id' };
+    }
+
+    // Get project title, description and admin
+    let projectQuery = await db.query('select title, description, user_id from projects where id=$1', [projectId]);
+    if (!projectQuery || !projectQuery.rows || projectQuery.rows.length <= 0) {
+      throw { code: 404, message: 'Project is not found' };
+    }
+    projectQuery = projectQuery.rows[0];
+
+    const projectInfo = {
+      projectId,
+      projectTitle: projectQuery.title,
+      projectDescription: projectQuery.description,
+      createdDate: projectQuery.create_date,
+      admin: projectQuery.user_id === id ? true : false
+    };
+
+    // Get project's users
+    const usersQuery = await db.query('select name, email from projects_users, users where users.id=projects_users.user_id AND project_id=$1', [projectId]);
+    if (!usersQuery || !usersQuery.rows || usersQuery.rows.length <= 0) {
+      projectInfo.users = [];
+    } else {
+      projectInfo.users = usersQuery.rows;
+    }
+
+    return projectInfo;
+  } catch (error) {
+    if (error.code) {
+      throw error;
+    }
+    const userMsg = 'Could not get project';
+    console.log(userMsg, error);
+    throw { code: 500, message: userMsg };
+  }
+};
+
 module.exports = {
-  newProject
+  newProject,
+  getProjects,
+  getProject
 };
