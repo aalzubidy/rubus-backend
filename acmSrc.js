eae15bf5223864a6b8a6da183e1f4add6a099ac9
Ahmed Al-Zubidy <aalzubidy90@gmail.com>
2020-12-03 09:44:52 -0800

    Modified acm

--- acmSrc.js
+++ acmSrc.js
@@ -1,150 +1,26 @@
-const testItem = `<li class="search__item issue-item-container">
-<div class="issue-item-checkbox-container"><label class="checkbox--primary"><input name="10.1145/3121245.3121247" class="issue-Item__checkbox" type="checkbox"><span></span></label></div>
-<div class="issue-item issue-item--search clearfix">
-   <div class="issue-item__citation">
-      <div class="issue-heading">ARTICLE</div>
-      
-      
-      
-      <div class="publisher-badge"><img class="simple-tooltip__block--b" src="/specs/products/acm/releasedAssets/images/acm-badge-20X20.png" alt="Published By ACM" title="Published By ACM" data-title="Published By ACM"><span class="visibility-hidden">Published By ACM</span></div>
-      <div class="badges"></div>
-   </div>
-   <div class="issue-item__content">
-      <div class="issue-item__content-right">
-         <h5 class="issue-item__title"><span class="hlFld-Title"><a href="/doi/10.1145/3121245.3121247">Hybrid monkey <span onclick="highlight()" class="single_highlight_class">testing</span>: enhancing automated GUI <span onclick="highlight()" class="single_highlight_class">tests</span> with random <span onclick="highlight()" class="single_highlight_class">test</span> generation</a></span></h5>
-         <ul class="rlist--inline loa truncate-list" aria-label="authors" data-lines="1">
-            <li><a href="/profile/99658665066" title="Thomas Wetzlmaier"><img class="author-picture" src="/pb-assets/icons/DOs/default-profile-1543932446943.svg" alt="Thomas &nbsp;Wetzlmaier profile image"/><span>Thomas Wetzlmaier</span></a><span>, </span></li>
-            <li><a href="/profile/81100138679" title="Rudolf Ramler"><img class="author-picture" src="/pb-assets/icons/DOs/default-profile-1543932446943.svg" alt="Rudolf &nbsp;Ramler profile image"/><span>Rudolf Ramler</span></a></li>
-         </ul>
-         <div class="issue-item__detail"><a href="/doi/proceedings/10.1145/3121245" title="A-TEST 2017: Proceedings of the 8th ACM SIGSOFT International Workshop on Automated Software Testing"><span class="epub-section__title">A-TEST 2017: Proceedings of the 8th ACM SIGSOFT International Workshop on Automated Software Testing</span></a><span class="dot-separator"><span>September 2017, </span><span>pp 5–10</span></span><span><a href="https://doi.org/10.1145/3121245.3121247" class="issue-item__doi dot-separator">https://doi.org/10.1145/3121245.3121247</a></span></div>
-         <div class="issue-item__abstract truncate-text" data-lines="4">
-            
-            <p> Many software projects maintain automated GUI tests that are repeatedly executed
-               for regression testing. Every test run executes exactly the same fixed sequence of
-               steps confirming that the currently tested version shows precisely the same behavior
-               as ...
-            </p>
-         </div>
-         <div class="issue-item__footer clearfix">
-            <div class="issue-item__footer-wrapper clearfix">
-               <div class="issue-item__footer-info pull-left">
-                  <ul class="rlist--inline">
-                     <li class="metric-holder">
-                        <div class="tooltip" tabindex="0">
-                           <ul class="rlist--inline padding-0">
-                              <li><span class="citation"><i class="icon-quote"></i><span>3</span></span></li>
-                              
-                              <li><span class="metric"><i class="icon-metric"></i><span>342</span></span></li>
-                              
-                           </ul>
-                           <div class="tooltip__body footer-metric-holder"><span class="arrow"></span><div class="left-bordered-title">Metrics</div>
-                              <div class="citation">Total Citations<span class="bold">3</span></div>
-                              
-                              <div class="metric">Total Downloads<span class="bold">342</span></div>
-                              <div class="info">Last 12 Months<span class="bold">199</span></div>
-                              <div class="info">Last 6 weeks<span class="bold">19</span></div>
-                              
-                           </div>
-                        </div>
-                     </li>
-                     <li class="highlights-holder">
-                        <div class="accordion"><a href="#" data-content-target="#accordion-content10114531212453121247" title="Show Highlights" aria-expanded="false" class="accordion__control"><i aria-hidden="true" class="icon-format_color_text"></i><span>Highlights</span><i aria-hidden="true" class="icon-section_arrow_d"></i></a></div>
-                     </li>
-                  </ul>
-                  <div class="accordion">
-                     <div id="accordion-content10114531212453121247" class="accordion__content" style=""><span class="hlFld-Abstract"></span><div class="abstract-text"><h6>Abstract</h6><p>Many  <span onclick="highlight()" class="single_highlight_class">software</span> projects maintain automated GUI  <span onclick="highlight()" class="single_highlight_class">tests</span> that are repeatedly executed for regression  <span onclick="highlight()" class="single_highlight_class">testing</span>. Every  <span onclick="highlight()" class="single_highlight_class">test</span> run executes exactly the same fixed sequence of steps confirming that the currently  <span onclick="highlight()" class="single_highlight_class">tested</span> version shows precisely the same behavior as the last version. The confirmatory approach implemented by these  <span onclick="highlight()" class="single_highlight_class">tests</span> limits their ability to find new defects. We therefore propose to combine existing automated regression  <span onclick="highlight()" class="single_highlight_class">tests</span> with random  <span onclick="highlight()" class="single_highlight_class">test</span> generation.</p> </div><div class="full-text"><h6>Full Text</h6><p>Hybrid Monkey  <span onclick="highlight()" class="single_highlight_class">Testing</span>: Enhancing Automated GUI  <span onclick="highlight()" class="single_highlight_class">Tests</span> with Random  <span onclick="highlight()" class="single_highlight_class">Test</span> Generation Thomas Wetzlmaier and Rudolf Ramler  <span onclick="highlight()" class="single_highlight_class">Software</span> Competence Center Hagenberg GmbH Softwarepark 21, A-4232 Hagenberg, Austria {thomas.wetzlmaier, rudolf.ramler}@scch.at Many  <span onclick="highlight()" class="single_highlight_class">software</span> projects maintain automated GUI  <span onclick="highlight()" class="single_highlight_class">tests</span> that are repeatedly executed for regression  <span onclick="highlight()" class="single_highlight_class">testing</span>. Every  <span onclick="highlight()" class="single_highlight_class">test</span> run executes exactly the same fixed sequence of steps confirming that the currently  <span onclick="highlight()" class="single_highlight_class">tested</span> version shows precisely the same behavior as the last version.</p><p>Running the enhanced  <span onclick="highlight()" class="single_highlight_class">tests</span> revealed three new defects. CCS CONCEPTS •  <span onclick="highlight()" class="single_highlight_class">Software</span> and its engineering →  <span onclick="highlight()" class="single_highlight_class">Software</span> verification and validation →  <span onclick="highlight()" class="single_highlight_class">Software</span>  <span onclick="highlight()" class="single_highlight_class">testing</span> and debugging KEYWORDS  <span onclick="highlight()" class="single_highlight_class">Software</span>  <span onclick="highlight()" class="single_highlight_class">test</span> automation, random  <span onclick="highlight()" class="single_highlight_class">test</span> generation, GUI  <span onclick="highlight()" class="single_highlight_class">testing</span>.</p> </div><div class="Subject"><h6>Subject</h6><p>A- <span onclick="highlight()" class="single_highlight_class">TEST</span>: Automating  <span onclick="highlight()" class="single_highlight_class">Test</span> Case Design, Selection and Evaluation</p><p><span onclick="highlight()" class="single_highlight_class">Software</span>  <span onclick="highlight()" class="single_highlight_class">testing</span> and debugging</p><p><span onclick="highlight()" class="single_highlight_class">Software</span> defect analysis</p> </div>
-                     </div>
-                  </div>
-               </div>
-               <div class="issue-item__footer-links pull-right">
-                  <ul class="rlist--inline left dot-separator">
-                     <li><a class="btn--icon simple-tooltip__block--b" href="#" data-title="Export Citation" data-toggle="modal" data-target="#exportCitation"><i class="icon-quote"></i><span class="visibility-hidden">Export Citations</span><input type="hidden" name="doiVal" value="10.1145/3121245.3121247"></a></li>
-                     <li><a href="/" role="menuitem" data-title="Save to Binder" data-db-target-for="save-to-binder-d3684e161" data-link="/action/binderList?doi=10.1145/3121245.3121247" class="btn--icon simple-tooltip__block--b saveToBinders"><i aria-hidden="true" class="icon-add-folder"></i><span class="visibility-hidden">Save to Binder</span></a><div data-db-target-of="save-to-binder-d3684e161">
-                           <div class="saveToBinders-list"><input type="hidden" name="doiVal" value="10.1145/3121245.3121247"><h5>Save to Binder</h5>
-                              <div class="binder-list-alerts"></div>
-                              <div class="saveToBinders-list__container">
-                                 <ul class="rlist saveToBinders-list__items">
-                                    <div class="loader"><img src="/specs/products/acm/releasedAssets/images/loader.gif"></div>
-                                 </ul>
-                              </div>
-                              <div class="binder__create"><a class="show-binder-form toggle-binder-form" data-action="show" href="/action/showMyBinders" title="Create a New Binder"><i class="icon-plus-light"></i>
-                                    Create a New Binder
-                                    </a><div class="binder-create-form binder__create-mini-form">
-                                    <div class="input-group input-group--gray-bg"><label>Name</label><div class="input-group__field-container--right-icon"><input type="text" placeholder="My new Binder" name="binderName" data-maxchar="256" class="binder-create-form__name charCount__text form-control"><span class="input-group-addon input-group-addon--right"><span class="charLimit"></span></span></div>
-                                    </div>
-                                    <div>
-                                       <ul class="rlist--inline binder__create-btns-container">
-                                          <li><button class="toggle-binder-form btn btn--border-less btn transparent binder__create-cancel" type="reset" value="Cancel" data-action="hide">Cancel
-                                                </button></li>
-                                          <li class="pull-right"><button class="btn btn--no-margin blue big binder__create-submit binder-create-form__create" type="submit" value="Create Binder" disabled data-append-newbinder-link="/action/createBinder"><i class="icon-plus-light" aria-hidden="true"></i>
-                                                Create
-                                                </button></li>
-                                       </ul>
-                                    </div>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </li>
-                  </ul>
-                  
-                  
-                  <ul class="rlist--inline right dot-separator">
-                     <li><a href="/doi/pdf/10.1145/3121245.3121247" title="Get Access" class="get-access"><i class="icon-get-access"></i><span>Get Access</span></a></li>
-                  </ul>
-                  
-                  <ul class="rlist--inline right">
-                     
-                  </ul>
-               </div>
-            </div>
-         </div>
-      </div>
-   </div>
-</div>
-</li>`;
-
 const axios = require('axios');
 const { html } = require('cheerio');
 const cheerio = require('cheerio');
 let $ = null;
 
 const test = async function test() {
-  // let cookieString = await axios('https://dl.acm.org/');
-  // cookieString = cookieString['headers']['set-cookie'][0];
+  let cookieString = await axios('https://dl.acm.org/');
+  cookieString = cookieString['headers']['set-cookie'][0];
 
-  // const results = await axios.get('https://dl.acm.org/action/doSearch?AllField=Software+AND+%28Quality+OR+Testing%29+NOT+Mobile', {
-  //   headers: {
-  //     'Cookie': cookieString
-  //   }
-  // });
-  // $ = cheerio.load(results.data);
+  const results = await axios.get('https://dl.acm.org/action/doSearch?AllField=Software+AND+%28Quality+OR+Testing%29+NOT+Mobile', {
+    headers: {
+      'Cookie': cookieString
+    }
+  });
+  $ = cheerio.load(results.data);
 
   const articles = [];
 
-  // $('.issue-item-container').each((i, item)=>{
-  //   const htmlItem = cheerio.load(item);
-  //   const title = htmlItem('.issue-item__title').text();
-  //   const doi = htmlItem('.issue-item__title a').attr('href');
-  //   const authors = htmlItem('[aria-label="authors"]').text().trim().replace(/\s\s/g, '').split(',');
-  //   const venue = htmlItem('.issue-item__detail a .epub-section__title').text(); 
-  //   let year =  htmlItem('.issue-item__detail .dot-separator').text().trim().split(' ');
-  //   year = `${year[0]} ${year[1]}`;
-  //   year = year.indexOf(',') > 0 ? year.substring(0, year.indexOf(',')) : year;
-  
-  //   articles.push({
-  //     doi,
-  //     title,
-  //     authors,
-  //     venue,
-  //     year
-  //   });
-  // });
-
-  const htmlItem = cheerio.load(testItem);
+  $('.issue-item-container').each((i, item)=>{
+    const htmlItem = cheerio.load(item);
     const title = htmlItem('.issue-item__title').text();
     const doi = htmlItem('.issue-item__title a').attr('href');
-    const authors = htmlItem('[aria-label="authors"]').text().trim().replace(/\s\s/g, '').split(',');
+    const authors = htmlItem('[aria-label="authors"]').text().trim().replace(/\s\s/g, '').replace(/,\s/g, ',').split(',');
     const venue = htmlItem('.issue-item__detail a .epub-section__title').text(); 
     let year =  htmlItem('.issue-item__detail .dot-separator').text().trim().split(' ');
     year = `${year[0]} ${year[1]}`;
@@ -157,6 +33,24 @@ const test = async function test() {
       venue,
       year
     });
+  });
+
+//   const htmlItem = cheerio.load(testItem);
+//     const title = htmlItem('.issue-item__title').text();
+//     const doi = htmlItem('.issue-item__title a').attr('href');
+//     const authors = htmlItem('[aria-label="authors"]').text().trim().replace(/\s\s/g, '').split(',');
+//     const venue = htmlItem('.issue-item__detail a .epub-section__title').text(); 
+//     let year =  htmlItem('.issue-item__detail .dot-separator').text().trim().split(' ');
+//     year = `${year[0]} ${year[1]}`;
+//     year = year.indexOf(',') > 0 ? year.substring(0, year.indexOf(',')) : year;
+  
+//     articles.push({
+//       doi,
+//       title,
+//       authors,
+//       venue,
+//       year
+//     });
 
   console.log(articles);
 };
